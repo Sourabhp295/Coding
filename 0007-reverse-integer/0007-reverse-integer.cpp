b4554198ae89c97ab5239f23ba9610058362fb5a
Sourabhp295 <122387208+Sourabhp295@users.noreply.github.com>
2023-01-13 15:54:55 +0530

    Time: 6 ms (26.02%), Space: 6 MB (10.25%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long  rev=0;
-        int rem=0;
+        long rev=0;
+       
         while(x != 0){
-            rem=x%10;
+            int rem=x%10;
             rev=rev*10+rem;
             x=x/10;
         }
